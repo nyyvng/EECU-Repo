b884f58e6509e34ecad8c8a4f5fb0042ab9b6c67
JordanG10000 <jordangoodall001@gmail.com>
2026-03-06 14:46:45 -0800

    Group inputs into sections and add toggle script

--- app.js
+++ app.js
@@ -10,3 +10,14 @@ customElements.define("cost-inputs", class extends HTMLElement { //Defines a cus
         this.shadowRoot.querySelector("label").textContent = this.getAttribute("name");
     }
 });
+
+const headers = document.querySelectorAll(".section-header");
+
+for (let header of headers) {
+    header.addEventListener("click", (e) => {
+        // add event listener to each, when it click change the panel class display
+        // if display == block set to none, else set to block
+        // this will hopefully allow the dropdowns to work 😁
+    })
+}
+
